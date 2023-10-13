b70ac11180bed81be23d43554c59290a3ea85671
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-13 15:41:03 +0200

    add constructeurs

--- Robots.h
+++ Robots.h
@@ -6,6 +6,8 @@
 class Robots : public Heros
 {
 public:
+    Robots(std::string nomheros, int vieheros, std::string nomarme, int puissancearme);
+    Robots(std::string nomheros, int vieheros, std::string nomarme, int puissancearme, int forcegadgets);
     void utiliserLesGadgets();
 private:
     int m_forcegadgets;
