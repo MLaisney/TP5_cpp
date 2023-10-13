f73372c118d2074b8aa99b0c51dddcfb4b735384
Mlaisney <laisney.melvyn@lyceejulesfil.fr>
2023-10-13 15:45:05 +0200

    add space

--- Jedis.cpp
+++ Jedis.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "Jedis.h"
 #include <string>
+
 using namespace std;
+
 Jedis::Jedis(string nomheros, int vieheros, string nomarme, int puissancearme): 
 Heros(nomheros, vieheros, nomarme, puissancearme), m_force(100) // Constructeur avec les attributs du Heros à déterminer et m_force a 100
 {
