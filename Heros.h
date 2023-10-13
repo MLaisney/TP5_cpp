5458a75d16330f01bc2444c859149a0112c382b9
MLaisney <laisney.melvyn@lyceejulesfil.com>
2023-10-13 22:28:43 +0200

    add 8

--- Heros.h
+++ Heros.h
@@ -16,7 +16,7 @@ void diminuerVie(int vieenmoins);
 void attaquer(Heros &cible);
 bool vivant() const;
 virtual void afficher() const;
-static void afficher4Heros(Heros &cible1,Heros &cible2 ,Heros &cible3 ,Heros &cible4);
+static void afficher8Heros(Heros &cible1,Heros &cible2 ,Heros &cible3 ,Heros &cible4 ,Heros &cible4 ,Heros &cible6 ,Heros &cible7 ,Heros &cible8);
 };
 
 #include "Heros.cpp"
